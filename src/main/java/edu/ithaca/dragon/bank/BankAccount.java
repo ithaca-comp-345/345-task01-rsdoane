3877487d1c54b8d60f366156252b55f59d0e8771
rsdoane <rsdoane99@gmail.com>
2021-02-02 22:45:39 -0500

    implemented isEmailValid from my repository

--- src/main/java/edu/ithaca/dragon/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/BankAccount.java
@@ -1,4 +1,5 @@
 package edu.ithaca.dragon.bank;
+import java.util.regex.Matcher;
 
 public class BankAccount {
 
@@ -27,11 +28,14 @@ public class BankAccount {
     }
 
     /**
-     * @post If the amnount is non-negative and is smaller than the balance, subtract the balance by the amount. Otherwise throw exception
+     * @post reduces the balance by amount if amount is non-negative and smaller than balance
      */
     public void withdraw (double amount) throws InsufficientFundsException{
         if (amount <= balance){
             balance -= amount;
+        }
+        else if(amount < 0){
+            throw new InsufficientFundsException("Cannot subtract negative money.");
         }
         else {
             throw new InsufficientFundsException("Not enough money");
@@ -40,10 +44,84 @@ public class BankAccount {
 
 
     public static boolean isEmailValid(String email){
-        if (email.indexOf('@') == -1){
+         if(!(email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$"))){
+            return false;
+        }
+        else if (email.indexOf('@') == -1){
+            return false;
+        }
+        else if (email.lastIndexOf('.') == (email.indexOf('@') + 1)){
+            return false;
+        }
+        else if (email.lastIndexOf('.') < (email.lastIndexOf('@'))){
+            return false;
+        }
+        else if ((email.length() - 1) == email.lastIndexOf('.')){
+            return false;
+        }
+        else if(email.charAt(email.lastIndexOf('@') - 1) == '-' || email.charAt(email.lastIndexOf('@') - 1) == '_' || email.charAt(email.lastIndexOf('@') - 1) == '.'){
+            return false;
+        }
+        else if(email.charAt(0) == '.' || email.charAt(0) == '-' || email.charAt(0) == '_'){
+            return false;
+        }
+        else if(email.charAt(email.lastIndexOf('.') - 1) == '.' || email.charAt(email.indexOf('.') + 1) == '.'){
+            return false;
+        }
+        else if(email.charAt(email.lastIndexOf('.') - 1) == '-' || email.charAt(email.indexOf('.') + 1) == '-'){
+            return false;
+        }
+        else if(email.charAt(email.lastIndexOf('.') - 1) == '_' || email.charAt(email.indexOf('.') + 1) == '_'){
+            return false;
+        }
+        else if(!doubleDash(email)){
+            return false;
+        }
+        else if(!doubleUnderscore(email)){
+            return false;
+        }
+        else if(email.charAt(email.lastIndexOf('.') + 1) == 't'){
             return false;
         }
         else {
+            return true;
+        }
+    }
+    public static boolean doubleDash(String email){
+        if(email.indexOf('-') != -1){
+            if(email.charAt(email.lastIndexOf('-') - 1) == '-' || email.charAt(email.indexOf('-') + 1) == '-'){
+                return false;
+            }
+            else if(email.charAt(email.lastIndexOf('-') - 1) == '.' || email.charAt(email.indexOf('-') + 1) == '.'){
+                return false;
+            }
+            else if(email.charAt(email.lastIndexOf('-') - 1) == '_' || email.charAt(email.indexOf('-') + 1) == '_'){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+        else{
+            return true;
+        }
+    }
+    public static boolean doubleUnderscore(String email){
+        if(email.indexOf('_') != -1){
+            if(email.charAt(email.lastIndexOf('_') - 1) == '_' || email.charAt(email.indexOf('_') + 1) == '_'){
+                return false;
+            }
+            else if(email.charAt(email.lastIndexOf('_') - 1) == '-' || email.charAt(email.indexOf('_') + 1) == '-'){
+                return false;
+            }
+            else if(email.charAt(email.lastIndexOf('_') - 1) == '.' || email.charAt(email.indexOf('_') + 1) == '.'){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+        else{
             return true;
         }
     }
